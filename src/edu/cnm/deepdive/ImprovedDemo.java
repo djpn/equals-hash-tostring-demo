d074f821987f1332c8bfc2f60c7f70fd379c55dc
Nick Bennett <nick@nickbenn.com>
2019-06-24 11:20:16 -0600

    Add explanatory comment for subclass serialVersionUID.

--- src/edu/cnm/deepdive/ImprovedDemo.java
+++ src/edu/cnm/deepdive/ImprovedDemo.java
@@ -2,7 +2,7 @@ package edu.cnm.deepdive;
 
 public class ImprovedDemo extends Demo {
 
-
+  // Even if a sublass doesn't add state fields, it should define its own serialVersionUID.
   private static final long serialVersionUID = -4569463404773389307L;
 
   public ImprovedDemo(int x, int y, double z) {
