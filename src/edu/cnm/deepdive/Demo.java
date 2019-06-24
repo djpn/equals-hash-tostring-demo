cb9a6bb9a8aab9e66be5bfb17fc1046b791746f9
Nick Bennett <nick@nickbenn.com>
2019-06-24 11:16:13 -0600

    More real-world alternative approaches.

--- src/edu/cnm/deepdive/Demo.java
+++ src/edu/cnm/deepdive/Demo.java
@@ -1,14 +1,25 @@
 package edu.cnm.deepdive;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Demo implements Serializable {
 
+  private static final long serialVersionUID = -5390786974047307421L;
+
   private final int x;
   private final int y;
   private final double z;
-  private transient double temp;
+
+  /*
+   * Transient fields ...
+   * - Aren't included in standard Java serialization/deserialization.
+   * - Are ignored by Gson & Jackson when serializing/deserializing to/from JSON.
+   * - Are ignored by Room & JPA (including Hibernate) in generating DDL and DML for data model.
+   */
+  private transient int hash;
+  private transient boolean hashComputed;
 
   public Demo(int x, int y, double z) {
     this.x = x;
@@ -18,20 +29,19 @@ public class Demo implements Serializable {
 
   @Override
   public int hashCode() {
-    return Objects.hash(x, y, z);
+    if (!hashComputed) {
+      hash = Objects.hash(state());
+      hashComputed = true;
+    }
+    return hash;
   }
 
   @Override
   public boolean equals(Object other) {
-    boolean result = false;
-    if (other != null
-        && other.getClass() == getClass()) {
-      Demo otherDemo = (Demo) other;
-      result = otherDemo.x == x
-          && otherDemo.y == y
-          && otherDemo.z == z;
-    }
-    return result;
+    return other != null
+        && other.getClass() == getClass()
+        && other.hashCode() == hashCode() // If we're following the recommended equals-hashCode contract, we can depend on this.
+        && Arrays.equals(((Demo) other).state(), state());
   }
 
   @Override
@@ -49,6 +59,10 @@ public class Demo implements Serializable {
 
   public double getZ() {
     return z;
+  }
+
+  protected Object[] state() {
+    return new Object[]{x, y, z};
   }
 
 }
--- src/edu/cnm/deepdive/ImprovedDemo.java
+++ src/edu/cnm/deepdive/ImprovedDemo.java
@@ -2,6 +2,9 @@ package edu.cnm.deepdive;
 
 public class ImprovedDemo extends Demo {
 
+
+  private static final long serialVersionUID = -4569463404773389307L;
+
   public ImprovedDemo(int x, int y, double z) {
     super(x, y, z);
   }
